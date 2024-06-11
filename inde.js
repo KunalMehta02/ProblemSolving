3d08d68465a97a5f9fd3846c773eddeeefb89719
kunalmehtaDeveloper <153451375+kunalmehtaDeveloper@users.noreply.github.com>
2024-06-11 20:21:32 +0530

    Event loop from scratch

--- inde.js
+++ inde.js
@@ -1,6 +1,25 @@
-console.log('Welcome git');
-console.log("hello");
-console.log('Welcome git');
-console.log("hello");
+// console.log('Welcome git');
+// console.log("hello");
+// console.log('Welcome git');
+// console.log("hello");
+
+
+
+// function f1(){
+// setTimeout(()=>{console.log('Welcome 5000');},5000);
+// setTimeout(()=>{console.log('Welcome 3000');},3000);
+// } 
+// f1();
+
+console.log('satrt');
+setTimeout(function cbT(){
+    console.log('cb setTimeout')
+},5000);
+
+fetch("https://www.netflix.com/in/")
+.then(function cbF(){
+    console.log("CB netflix");
+});
+console.log("end")
 
 
